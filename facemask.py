def1fbbcb1c99719c0e5d0f279c32728ffc5f4f8
Preeti Rana <66157089+priyasi345@users.noreply.github.com>
2020-07-23 23:51:53 -0700

    Add files via upload

---
+++ facemask.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from PIL import Image
+import cv2
+import numpy as np
+from imutils import face_utils
+import dlib
+
+
+# In[ ]:
+
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("C:\\Users\\PREETI\\Desktop\\shape_predictor_68_face_landmarks.dat")
+
+cap = cv2.VideoCapture(0)
+while (True):
+    bol, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 0)
+    for (i, rect) in enumerate(rects):
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+        for (x, y) in shape:
+            cv2.circle(img, (x, y), 2, (0, 255, 0), -1)
+    
+    cv2.imshow("image",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
